53b8754b8474feae3cecf553b9f649908c4e7b96
HexPang <hexpang@jabstruse.com>
2016-08-25 14:23:36 +0800

    Add CloudAPIClient.

---
+++ src/hexpang/Client/CloudAPIClient.php
@@ -0,0 +1,34 @@
+<?php
+namespace hexpang\Client;
+
+class CloudAPIClient{
+    var $app_id;
+    var $API_URL;
+    public function __construct($app_id)
+    {
+      $this->API_URL = 'http://localhost:8000/';
+        $this->app_id = $app_id;
+    }
+
+    public function Request($key,$data){
+        $url = $this->API_URL . "api/{$this->app_id}/{$key}/";
+        $data = json_encode($data);
+        $data = urlencode($data);
+        $sign = md5($this->app_id . $data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,['data'=>$data,'sign'=>$sign]);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+}
+?>
